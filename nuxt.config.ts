bc1f7e8b10551a91e8f7946650c416188364d708
Khalid Mohammad <khalid.m.sheet@gmail.com>
2023-01-16 11:14:55 +0300

    just added a page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
